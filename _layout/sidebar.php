1828abaf0267478815b4a0f6376a341cf0b2fd91
Achmad Chadil Auwfar <achmad.chadilauwfar@gmail.com>
2016-10-12 13:40:56 +0700

    Add files via upload

---
+++ _layout/sidebar.php
@@ -0,0 +1,50 @@
+				<section id="sidebar">
+					<div class="module">
+				        <h3 class="module-title">
+				            About Me
+				        </h3>
+				        <div class="module-content">
+				        	<div class="module-profile">
+					        	<img src="assets/img/profil.jpg" alt="">
+					        	<div class="module-profile-content">
+						            <h3>Achmad Chadil Auwfar</h3>
+						            <p>
+						                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos veniam id fugit, voluptatum maxime a laborum possimus, obcaecati
+						            </p>
+					        	</div>
+				        	</div>
+				        </div>
+				    </div>
+
+				    <div class="module">
+				        <h3 class="module-title">
+				            Latest News
+				        </h3>
+				        <div class="module-content">
+				            <ul>
+				                <li><a href="">Lorem Ipsum</a></li>
+				                <li><a href="">Lorem Ipsum</a></li>
+				                <li><a href="">Lorem Ipsum</a></li>
+				                <li><a href="">Lorem Ipsum</a></li>
+				            </ul>
+				        </div>
+				    </div>
+
+				    <div class="module">
+				        <h3 class="module-title">
+				            Popular News
+				        </h3>
+				        <div class="module-content">
+				        	<div class="module-popular">
+					        	<img src="assets/img/3.jpg" alt="">
+					        	<div class="module-popular-content">
+						            <h3>Judul Artikel</h3>
+						            <p>
+						                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos veniam id fugit, voluptatum maxime a laborum possimus, obcaecati
+						            </p>
+					        	</div>
+				        	</div>
+				        </div>
+				    </div>
+				</section>
+				<div class="clearfix"></div>
